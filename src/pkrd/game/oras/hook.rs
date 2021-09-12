fd8237bde369530fac3cb995893566d6c10b31a3
Admiral-Fish <admiralfish420@gmail.com>
2021-09-12 14:21:33 -0700

    Gen 6 initial seed patch (#11)

--- src/pkrd/game/oras/hook.rs
+++ src/pkrd/game/oras/hook.rs
@@ -38,6 +38,9 @@ impl HookableProcess for PokemonORAS {
             present_framebuffer_addr: 0x148758,
             hook_vars_addr: 0x5d0000,
         };
-        Self::patch_present_framebuffer(process, pkrd_handle, config)
+        let inital_seed_address = 0x12e5c8;
+
+        Self::patch_present_framebuffer(process, pkrd_handle, config);
+        Self::patch_inital_seed(process, inital_seed_address)
     }
 }

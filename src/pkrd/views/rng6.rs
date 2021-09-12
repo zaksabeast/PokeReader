7b0dd566082284c16ded457a12e8d1dda11f9b11
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2021-09-11 19:47:50 -0500

    Format rng views

--- src/pkrd/views/rng6.rs
+++ src/pkrd/views/rng6.rs
@@ -12,19 +12,16 @@ pub fn run_view(
         let black = display::Color::black();
         let white = display::Color::white();
 
-        let mt_state_index = game.get_mt_state_index()?;
-
         screen.paint_square(&black, x, y, 200, 32)?;
+
         x += 10;
         y += 4;
         screen.draw_string(&white, "Hello from rust!", x, y)?;
+
         y += 16;
-        screen.draw_string(
-            &white,
-            &alloc::format!("MT state index {}", mt_state_index),
-            x,
-            y,
-        )?;
+        let mt_state_index = game.get_mt_state_index()?;
+        let state_text = &alloc::format!("MT state index {}", mt_state_index);
+        screen.draw_string(&white, state_text, x, y)?;
     }
 
     Ok(())
--- src/pkrd/views/rng7.rs
+++ src/pkrd/views/rng7.rs
@@ -12,23 +12,22 @@ pub fn run_view(
         let black = display::Color::black();
         let white = display::Color::white();
 
-        let init_seed = game.get_initial_seed();
-
         screen.paint_square(&black, x, y, 200, 56)?;
+
         x += 10;
         y += 4;
         screen.draw_string(&white, "Hello from rust!", x, y)?;
+
         y += 12;
         screen.draw_string(&white, "Official Luma", x, y)?;
+
         y += 12;
         screen.draw_string(&white, "Not NTR", x, y)?;
+
         y += 16;
-        screen.draw_string(
-            &white,
-            &alloc::format!("Init seed: {:08x}", init_seed),
-            x,
-            y,
-        )?;
+        let init_seed = game.get_initial_seed();
+        let seed_text = &alloc::format!("Init seed: {:08x}", init_seed);
+        screen.draw_string(&white, seed_text, x, y)?;
     }
 
     Ok(())

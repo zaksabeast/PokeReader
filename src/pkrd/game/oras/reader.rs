79c35b6662ac5bd70028a19e15eb541a6a5ae4c9
Steve Cook <steviecook210@gmail.com>
2022-05-29 11:54:26 -0400

    Linting

--- src/pkrd/game/oras/reader.rs
+++ src/pkrd/game/oras/reader.rs
@@ -42,7 +42,7 @@ impl Gen6Reader for PokemonORASReader {
 
     fn get_wild_offset(&self) -> usize {
         let mut pointer = self.default_read::<u32>(0x80313C) - 0x22C0;
-        if pointer < 0x8000000 || pointer > 0x8DF0000 {
+        if !(0x8000000..=0x8DF0000).contains(&pointer) {
             if self.default_read::<u32>(0x804060) == 0 {
                 0x804064
             } else {
@@ -50,7 +50,7 @@ impl Gen6Reader for PokemonORASReader {
             }
         } else {
             pointer = self.default_read::<u32>((pointer - 0x8000000) as usize);
-            if pointer < 0x8000000 || pointer > 0x8DF0000 {
+            if !(0x8000000..=0x8DF0000).contains(&pointer) {
                 if self.default_read::<u32>(0x804060) == 0 {
                     0x804064
                 } else {
--- src/pkrd/game/xy/reader.rs
+++ src/pkrd/game/xy/reader.rs
@@ -42,11 +42,11 @@ impl Gen6Reader for PokemonXYReader {
 
     fn get_wild_offset(&self) -> usize {
         let mut pointer = self.default_read::<u32>(0x80313C) - 0xA1C;
-        if pointer < 0x8000000 || pointer > 0x8DF0000 {
+        if !(0x8000000..=0x8DF0000).contains(&pointer) {
             0x805614
         } else {
             pointer = self.default_read::<u32>(pointer as usize);
-            if pointer < 0x8000000 || pointer > 0x8DF0000 {
+            if !(0x8000000..=0x8DF0000).contains(&pointer) {
                 0x805614
             } else {
                 pointer as usize

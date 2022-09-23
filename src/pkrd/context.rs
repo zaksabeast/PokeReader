a75688dd8a83937a72f27e03abb5e122c21f5338
SteveCookTU <steviecook210@gmail.com>
2022-09-23 08:30:06 -0400

    Update libctr-rs (#32)

--- src/pkrd/context.rs
+++ src/pkrd/context.rs
@@ -2,8 +2,9 @@ use super::{
     display::{DirectWriteScreen, Screen},
     hook,
 };
+use crate::PkrdGameCommand;
 use alloc::boxed::Box;
-use ctr::{res::CtrResult, sysmodule::server::ServiceContext};
+use ctr::{match_ctr_route, res::CtrResult, sysmodule::server::ServiceRouter};
 
 pub struct PkrdServiceContext {
     pub screen: DirectWriteScreen,
@@ -21,4 +22,22 @@ impl PkrdServiceContext {
     }
 }
 
-impl ServiceContext for PkrdServiceContext {}
+impl ServiceRouter for PkrdServiceContext {
+    fn accept_session(&mut self, _session_index: usize) {}
+
+    fn close_session(&mut self, _session_index: usize) {}
+
+    fn handle_request(
+        &mut self,
+        service_id: usize,
+        session_index: usize,
+    ) -> CtrResult<ctr::ipc::WrittenCommand> {
+        match_ctr_route!(
+            PkrdServiceContext,
+            service_id,
+            session_index,
+            PkrdGameCommand::Setup,
+            PkrdGameCommand::RunGameHook
+        )
+    }
+}
--- src/pkrd/request_handler.rs
+++ src/pkrd/request_handler.rs
@@ -1,20 +1,20 @@
 use super::{context::PkrdServiceContext, display::Screen, frame_pause::handle_frame_pause, hook};
 use crate::{log, pkrd::notification};
-use alloc::format;
 use core::{
     mem, slice,
     sync::atomic::{AtomicU32, Ordering},
 };
 use ctr::{
-    hid,
+    ctr_method, hid,
     hid::InterfaceDevice,
-    ipc,
-    res::{GenericResultCode, ResultCode},
+    ipc::Handles,
+    res::{CtrResult, GenericResultCode, ResultCode},
     svc,
-    sysmodule::server,
+    sysmodule::server::Service,
     Handle,
 };
-use num_enum::IntoPrimitive;
+use no_std_io::{EndianRead, EndianWrite};
+use num_enum::{FromPrimitive, IntoPrimitive};
 
 static PKRD_HANDLE: AtomicU32 = AtomicU32::new(0);
 
@@ -26,112 +26,91 @@ pub fn get_pkrd_session_handle() -> mem::ManuallyDrop<Handle> {
     mem::ManuallyDrop::new(handle)
 }
 
-#[derive(IntoPrimitive)]
+#[derive(IntoPrimitive, FromPrimitive)]
 #[repr(u16)]
-enum PkrdGameCommand {
+pub enum PkrdGameCommand {
+    #[num_enum(default)]
     InvalidCommand = 0,
     Setup = 1,
     RunGameHook = 2,
 }
 
-impl From<u16> for PkrdGameCommand {
-    fn from(value: u16) -> Self {
-        match value {
-            1 => Self::Setup,
-            2 => Self::RunGameHook,
-            _ => Self::InvalidCommand,
-        }
-    }
+impl Service for PkrdGameCommand {
+    const ID: usize = 0;
+    const NAME: &'static str = "pkrd:game";
+    const MAX_SESSION_COUNT: i32 = 8;
 }
 
-pub fn handle_pkrd_game_request(
+#[ctr_method(cmd = "PkrdGameCommand::Setup", normal = 0x1, translate = 0x0)]
+fn setup(_context: &mut PkrdServiceContext, _session_index: usize, handles: Handles) -> CtrResult {
+    PKRD_HANDLE.store(handles.into_raw()[0], Ordering::Relaxed);
+    Ok(())
+}
+
+#[derive(EndianRead, EndianWrite)]
+struct GameHookParams {
+    frame_buffer: u32,
+    screen_id: u32,
+    stride: u32,
+    format: u32,
+    heap_ptr: u32,
+    heap_len: u32,
+}
+
+#[ctr_method(cmd = "PkrdGameCommand::RunGameHook", normal = 0x1, translate = 0x0)]
+fn run_game_hook(
     context: &mut PkrdServiceContext,
-    mut command_parser: ipc::ThreadCommandParser,
     _session_index: usize,
-) -> server::RequestHandlerResult {
-    let command_id = command_parser.get_command_id();
+    params: GameHookParams,
+) -> CtrResult {
+    let is_top_screen = params.screen_id == 0;
 
-    log::debug(&format!(
-        "[CMD] pkrd:game[{:x}][{:x}]",
-        command_id,
-        command_parser.get_header()
-    ));
+    if notification::is_new_game_launch() {
+        // Get heap
+        let heap_ptr = params.heap_ptr as *mut u8;
+        let heap_len = params.heap_len as usize;
 
-    match command_id.into() {
-        PkrdGameCommand::Setup => {
-            command_parser.pop();
-            let raw_handle = command_parser.pop();
-            PKRD_HANDLE.store(raw_handle, Ordering::Relaxed);
+        // We're trusting our patch works and nothing else is using this command
+        let physical_heap_ptr = svc::convert_pa_to_uncached_pa(heap_ptr)?;
+        let heap = unsafe { slice::from_raw_parts_mut(physical_heap_ptr, heap_len) };
 
-            let mut command = ipc::ThreadCommandBuilder::new(command_id);
-            command.push(ResultCode::success());
-            Ok(command.build())
-        }
-        PkrdGameCommand::RunGameHook => {
-            // Check to make sure we're getting what we're expecting
-            command_parser.validate_header(PkrdGameCommand::RunGameHook, 6, 0)?;
-
-            // Get screen props
-            let frame_buffer = command_parser.pop();
-            let screen_id = command_parser.pop();
-            let stride = command_parser.pop();
-            let format = command_parser.pop();
-            let is_top_screen = screen_id == 0;
-
-            if notification::is_new_game_launch() {
-                // Get heap
-                let heap_ptr = command_parser.pop() as *mut u8;
-                let heap_len = command_parser.pop_usize();
-
-                // We're trusting our patch works and nothing else is using this command
-                let physical_heap_ptr = svc::convert_pa_to_uncached_pa(heap_ptr)?;
-                let heap = unsafe { slice::from_raw_parts_mut(physical_heap_ptr, heap_len) };
-
-                // Since this is a physical address, it is static memory
-                context.game = hook::get_hooked_process(heap);
-            }
-
-            let screen = &mut context.screen;
-
-            if let Err(result_code) =
-                screen.set_context(is_top_screen, frame_buffer, stride, format)
-            {
-                log::error(&alloc::format!(
-                    "Failed screen context {:x}",
-                    result_code.into_raw()
-                ));
-            }
-
-            // The input needs to be scanned here so we can use it in the hook
-            hid::Global::scan_input();
-
-            let hook_result = context
-                .game
-                .as_mut()
-                .ok_or_else::<ResultCode, fn() -> ResultCode>(|| {
-                    GenericResultCode::InvalidValue.into()
-                })?
-                .run_hook(screen);
-
-            // The game ignores the result of this, and there's not much we can
-            // do to handle the error aside from logging.
-            if let Err(result_code) = hook_result {
-                log::error(&alloc::format!(
-                    "Failed run_hook: {:x}",
-                    result_code.into_raw()
-                ));
-            }
-
-            handle_frame_pause(context, is_top_screen);
-
-            let mut command = ipc::ThreadCommandBuilder::new(command_id);
-            command.push(ResultCode::success());
-            Ok(command.build())
-        }
-        _ => {
-            let mut command = ipc::ThreadCommandBuilder::new(PkrdGameCommand::InvalidCommand);
-            command.push(GenericResultCode::InvalidCommand);
-            Ok(command.build())
-        }
+        // Since this is a physical address, it is static memory
+        context.game = hook::get_hooked_process(heap);
     }
+
+    let screen = &mut context.screen;
+
+    if let Err(result_code) = screen.set_context(
+        is_top_screen,
+        params.frame_buffer,
+        params.stride,
+        params.format,
+    ) {
+        log::error(&alloc::format!(
+            "Failed screen context {:x}",
+            result_code.into_raw()
+        ));
+    }
+
+    // The input needs to be scanned here so we can use it in the hook
+    hid::Global::scan_input();
+
+    let hook_result = context
+        .game
+        .as_mut()
+        .ok_or_else::<ResultCode, fn() -> ResultCode>(|| GenericResultCode::InvalidValue.into())?
+        .run_hook(screen);
+
+    // The game ignores the result of this, and there's not much we can
+    // do to handle the error aside from logging.
+    if let Err(result_code) = hook_result {
+        log::error(&alloc::format!(
+            "Failed run_hook: {:x}",
+            result_code.into_raw()
+        ));
+    }
+
+    handle_frame_pause(context, is_top_screen);
+
+    Ok(())
 }

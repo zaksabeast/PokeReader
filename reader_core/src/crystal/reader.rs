3c08a859ff7b702a36513b2e9809925b790aefbe
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2025-02-24 18:05:38 -0600

    Use offsets for gen 2 reader and fix dst

--- reader_core/src/crystal/reader.rs
+++ reader_core/src/crystal/reader.rs
@@ -6,12 +6,20 @@ struct Gen2Addresses {
     div_ptr: u32,
     pc_reg_ptr: u32,
     gb_rng_ptr: u32,
+    dst_ptr: u32,
+    time_ptr: u32,
+    time_day_ptr: u32,
+    play_time_ptr: u32,
 }
 
 const CRYSTAL_ADDRESSES: Gen2Addresses = Gen2Addresses {
     div_ptr: 0x22f794,
     pc_reg_ptr: 0x22f5fc,
     gb_rng_ptr: 0xffe1,
+    dst_ptr: 0xd4c2,
+    time_ptr: 0xff94,
+    time_day_ptr: 0xd4cb,
+    play_time_ptr: 0xd4c5,
 };
 
 pub struct Gen2Reader {
@@ -54,34 +62,34 @@ impl Gen2Reader {
     }
 
     pub fn time_seconds(&self) -> u8 {
-        gb_mem::read_u8(0xff98)
+        gb_mem::read_u8(self.addrs.time_ptr + 4)
     }
 
     pub fn time_minutes(&self) -> u8 {
-        gb_mem::read_u8(0xff96)
+        gb_mem::read_u8(self.addrs.time_ptr + 2)
     }
 
     pub fn time_hours(&self) -> u8 {
-        gb_mem::read_u8(0xff94)
+        gb_mem::read_u8(self.addrs.time_ptr)
     }
 
     pub fn time_day(&self) -> u8 {
-        gb_mem::read_u8(0xd4cb) % 7
-    }
-
-    pub fn dst(&self) -> bool {
-        gb_mem::read_u8(0xd4bc) != 0
+        gb_mem::read_u8(self.addrs.time_day_ptr) % 7
     }
 
     pub fn play_seconds(&self) -> u8 {
-        gb_mem::read_u8(0xD4C7)
+        gb_mem::read_u8(self.addrs.play_time_ptr + 2)
     }
 
     pub fn play_minutes(&self) -> u8 {
-        gb_mem::read_u8(0xD4C6)
+        gb_mem::read_u8(self.addrs.play_time_ptr + 1)
     }
 
     pub fn play_hours(&self) -> u8 {
-        gb_mem::read_u8(0xD4C5)
+        gb_mem::read_u8(self.addrs.play_time_ptr)
+    }
+
+    pub fn dst(&self) -> bool {
+        (gb_mem::read_u8(self.addrs.dst_ptr) & 0x80) != 0
     }
 }
